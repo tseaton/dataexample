01eaf8b727b47dc358d5ffcd15c582a46ad4dca4
Tyler Seaton <v-tyseaton@expedia.com>
2015-05-26 21:45:34 -0700

    Example working. Added two searches

--- src/main/java/com/dev9/dataexample/repo/SprocketRepository.java
+++ src/main/java/com/dev9/dataexample/repo/SprocketRepository.java
@@ -3,11 +3,15 @@ package com.dev9.dataexample.repo;
 
 import com.dev9.dataexample.entity.Sprocket;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface SprocketRepository extends CrudRepository<Sprocket, Long> {
 
-    Sprocket findBySku(String sku);
+    Sprocket findBySku(@Param("sku") String sku);
+    List<Sprocket> findByBrand(@Param("brand") String brand);
 
 }
