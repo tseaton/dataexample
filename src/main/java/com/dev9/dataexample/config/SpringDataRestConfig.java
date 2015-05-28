5f6023bfbabc1fe93b23c10a5c73769a4c8f6e88
Tyler Seaton <v-tyseaton@expedia.com>
2015-05-27 17:05:43 -0700

    Example showing the rest configuration

---
+++ src/main/java/com/dev9/dataexample/config/SpringDataRestConfig.java
@@ -0,0 +1,16 @@
+package com.dev9.dataexample.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
+
+@Configuration
+@Import(RepositoryRestMvcConfiguration.class)
+public class SpringDataRestConfig extends RepositoryRestMvcConfiguration {
+
+//    @Override
+//    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration repositoryRestConfiguration) {
+//        repositoryRestConfiguration.setBaseUri("inventory/");
+//    }
+}
